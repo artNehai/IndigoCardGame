5d7dd8a99a32f81ce7ab77a2343a093a7d9849d4
artNehay <artemynehay@gmail.com>
2023-07-26 20:24:57 +0300

    Stage 2: Implemented Card deck using classes and enum

--- src/indigo/Main.kt
+++ src/indigo/Main.kt
@@ -1,10 +1,8 @@
 package indigo
 
-import kotlin.ArithmeticException
-
 const val CARDS_NUM = 52
 
-enum class Ranks(val rank: String) {
+enum class Ranks(private val rank: String) {
     ACE("A"),
     TWO("2"),
     THREE("3"),
@@ -17,25 +15,30 @@ enum class Ranks(val rank: String) {
     TEN("10"),
     JACK("J"),
     QUEEN("Q"),
-    KING("K"),
+    KING("K");
+
+    override fun toString() = rank
 }
 
-enum class Suits(val suit: String) {
+enum class Suits(private val suit: String) {
     DIAMONDS("♦"),
     HEARTS("♥"),
     SPADES("♠"),
-    CLUBS("♣"),
+    CLUBS("♣");
+
+    override fun toString() = suit
 }
+
+val initialCardDeck: List<String> =
+    Ranks.values().flatMap { rank ->
+        Suits.values().map { suit ->
+            "$rank$suit"
+        }
+    }
+
 
 
 class Deck {
-
-    private val initialCardDeck: List<String> =
-        Ranks.values().flatMap { rank ->
-            Suits.values().map { suit ->
-                "${rank.rank}${suit.suit}"
-            }
-        }
 
     private var cardDeck: List<String> = initialCardDeck
 
@@ -49,7 +52,7 @@ class Deck {
         println("Card deck is shuffled.")
     }
 
-    fun get() {
+    fun getCard(): Unit {
 
         println("Number of cards:")
         val quantity: Int
@@ -57,9 +60,9 @@ class Deck {
         try {
             quantity = readln().toInt()
             if (quantity !in 1..CARDS_NUM) {
-                throw ArithmeticException()
+                throw NumberFormatException()
             }
-        } catch (e: ArithmeticException) {
+        } catch (e: NumberFormatException) {
             println("Invalid number of cards.")
             return
         }
@@ -77,6 +80,7 @@ class Deck {
 }
 
 
+
 fun main() {
 
     val cardDeck = Deck()
@@ -86,7 +90,7 @@ fun main() {
         when (readln().lowercase()) {
             "reset" -> cardDeck.reset()
             "shuffle" -> cardDeck.shuffle()
-            "get" -> cardDeck.get()
+            "get" -> cardDeck.getCard()
             "exit" -> {
                 println("Bye")
                 break
